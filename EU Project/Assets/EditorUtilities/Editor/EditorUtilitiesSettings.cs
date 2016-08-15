0da940e937aff1a16c78011946c8ee515092b158
lPinchol <pinchodev91@gmail.com>
2016-08-15 23:48:43 +0200

    Update tools editor

---
+++ EU Project/Assets/EditorUtilities/Editor/EditorUtilitiesSettings.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class EditorUtilitiesSettings : ScriptableObject
+{
+    public bool showEditorState = true;
+    public bool showSceneShortcuts = true;
+    public List<string> launchSceneShortcuts = new List<string>();
+    public List<string> workingSceneShortcuts = new List<string>();
+}
